a8998d61adfaf863c8eca85aa2e4346b70adfa90
Ella Schneyer <79551618+eschneyer@users.noreply.github.com>
2022-07-14 22:35:57 -0400

    Create contentData.js

---
+++ web/utils/contentData.js
@@ -0,0 +1,24 @@
+const contentData = [
+  {
+    title: "\"\Wave\"\ Variability",
+    description:
+    "Our program allows the user to select between various distances that they would consider safe or that are currently in line with official CDC guidelines during unique waves of infection."
+  },
+  {
+    title: "Unlimited Recognition Ability",
+    description:
+    "Three's A Company, right? No matter how many people you need to safely distance, our program can accomodate for it."
+  },
+  {
+    title: "One of A Kind",
+    description:
+    "Currently, our program is one of the very few apps to regulate social distance among groups of people."
+  },
+  {
+    title: "Safe & Sound",
+    description:
+    "By using Auth0 login authentication, you can easily create an account with google and ensure cybersecurity when logging in to our program"
+  }
+];
+
+export default contentData;
