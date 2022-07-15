e5b4049420b6831b99d41614e926bde2df5ba762
Ella Schneyer <79551618+eschneyer@users.noreply.github.com>
2022-07-14 22:32:41 -0400

    Create Footer.js

---
+++ web/components/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => (
+  <footer className="bg-light p-3 text-center">
+    <div className="logo" />
+    <p>
+    Software developed by Kenny Ge, Ella Schneyer, Sid Karthik, and Arjun Rajdev (Feb 2022 ©)
+    </p>
+    <p>Website template provided by <a href = "https://auth0.com/" >Auth0</a> with customizations by Ella Schneyer</p>
+  </footer>
+);
+
+export default Footer;
