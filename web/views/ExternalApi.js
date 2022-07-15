b5cabba871bef98908016be9e8e23aa1ce83da68
Ella Schneyer <79551618+eschneyer@users.noreply.github.com>
2022-07-14 22:39:08 -0400

    Create ExternalApi.js

---
+++ web/views/ExternalApi.js
@@ -0,0 +1,157 @@
+import React, { useState } from "react";
+import { Button, Alert } from "reactstrap";
+import Highlight from "../components/Highlight";
+import { useAuth0, withAuthenticationRequired } from "@auth0/auth0-react";
+import { getConfig } from "../config";
+import Loading from "../components/Loading";
+
+export const ExternalApiComponent = () => {
+  const { apiOrigin = "http://localhost:3001", audience } = getConfig();
+
+  const [state, setState] = useState({
+    showResult: false,
+    apiMessage: "",
+    error: null,
+  });
+
+  const {
+    getAccessTokenSilently,
+    loginWithPopup,
+    getAccessTokenWithPopup,
+  } = useAuth0();
+
+  const handleConsent = async () => {
+    try {
+      await getAccessTokenWithPopup();
+      setState({
+        ...state,
+        error: null,
+      });
+    } catch (error) {
+      setState({
+        ...state,
+        error: error.error,
+      });
+    }
+
+    await callApi();
+  };
+
+  const handleLoginAgain = async () => {
+    try {
+      await loginWithPopup();
+      setState({
+        ...state,
+        error: null,
+      });
+    } catch (error) {
+      setState({
+        ...state,
+        error: error.error,
+      });
+    }
+
+    await callApi();
+  };
+
+  const callApi = async () => {
+    try {
+      const token = await getAccessTokenSilently();
+
+      const response = await fetch(`${apiOrigin}/api/external`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+
+      const responseData = await response.json();
+
+      setState({
+        ...state,
+        showResult: true,
+        apiMessage: responseData,
+      });
+    } catch (error) {
+      setState({
+        ...state,
+        error: error.error,
+      });
+    }
+  };
+
+  const handle = (e, fn) => {
+    e.preventDefault();
+    fn();
+  };
+
+  return (
+    <>
+      <div className="mb-5">
+        {state.error === "consent_required" && (
+          <Alert color="warning">
+            You need to{" "}
+            <a
+              href="#/"
+              class="alert-link"
+              onClick={(e) => handle(e, handleConsent)}
+            >
+              consent to get access to users api
+            </a>
+          </Alert>
+        )}
+
+        {state.error === "login_required" && (
+          <Alert color="warning">
+            You need to{" "}
+            <a
+              href="#/"
+              class="alert-link"
+              onClick={(e) => handle(e, handleLoginAgain)}
+            >
+              log in again
+            </a>
+          </Alert>
+        )}
+
+        <h1>Downloads</h1>
+        <p className="lead">
+        Please download the software the correlates with your needs and/or current <a href="https://www.cdc.gov/">CDC guidelines</a> by clicking the button below. Please note that you will need to have <a href="https://www.python.org/">Python</a> and <a href="https://opencv.org/">OpenCV</a> downloaded to your computer for the applications to run smoothly in your computer's terminal.
+        </p>
+
+        <p>
+        Currently, we offer two versions of Covistance: a version that operates with 3ft between users as a minimum safe distance, and a version that operates with 6ft between users as a minimum safe distance.
+        </p>
+
+        <Button
+          color="primary"
+          className="mt-5"
+          onClick={() => window.open('6ft.zip')}
+        >
+          Covistance (6 ft)
+        </Button>
+        <Button
+          color="primary"
+          className="mt-5"
+          onClick={() => window.open('3ft.zip')}
+        >
+          Covistance (3 ft)
+        </Button>
+      </div>
+
+      <div className="result-block-container">
+        {state.showResult && (
+          <div className="result-block" data-testid="api-result">
+            <h6 className="muted">Result</h6>
+            <Highlight>
+              <span>{JSON.stringify(state.apiMessage, null, 2)}</span>
+            </Highlight>
+          </div>
+        )}
+      </div>
+    </>
+  );
+};
+
+export default withAuthenticationRequired(ExternalApiComponent, {
+  onRedirecting: () => <Loading />,
+});
